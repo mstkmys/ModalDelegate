3d035bbaa5e9b1cff2749028be490e76c5ac6ac1
Masataka Miyoshi <masataka.ehime.univ@gmail.com>
2018-04-11 04:31:52 +0900

    初期設定を追加

---
+++ ModalDelegate/Classes/AppDelegate.swift
@@ -0,0 +1,34 @@
+//
+//  AppDelegate.swift
+//  ModalDelegate
+//
+//  Created by Miyoshi Masataka on 2018/04/11.
+//  Copyright © 2018年 Masataka Miyoshi. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
